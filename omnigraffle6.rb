3b226d67be6c302bf9f5b188ae5390e4e909362a
Rob Ruth <rruth@robruth.com>
2018-06-06 12:53:53 -0700

    Update omnigraffle6.rb

--- omnigraffle6.rb
+++ omnigraffle6.rb
@@ -1,6 +1,6 @@
 cask 'omnigraffle' do
   version '6.6.2'
-  sha256 '0ddf72859b3ded2a251d57ffc1b01cc23fa361c2ccbe2438e807ed6238d4a51d'
+  sha256 '8ec42619d9e3df190678508ac5037d8aef55000c'
 
   url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniGraffle-#{version}.dmg"
   name 'OmniGraffle'
