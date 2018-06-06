25a5822c596fb3c96a28a1314afba9d16d49a71d
Rob Ruth <rruth@robruth.com>
2018-06-06 12:42:14 -0700

    Create omnigraffle6.rb

---
+++ omnigraffle6.rb
@@ -0,0 +1,14 @@
+cask 'omnigraffle' do
+  version '6.6.2'
+  sha256 '0ddf72859b3ded2a251d57ffc1b01cc23fa361c2ccbe2438e807ed6238d4a51d'
+
+  url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniGraffle-#{version}.dmg"
+  name 'OmniGraffle'
+  homepage 'https://www.omnigroup.com/omnigraffle/'
+
+  depends_on macos: '>= :sierra'
+
+  app 'OmniGraffle.app'
+
+  zap trash: '~/Library/Application Support/The Omni Group/OmniGraffle'
+end
